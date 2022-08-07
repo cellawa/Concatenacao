d40ffc269682bc35ece7df8739cfeca581798820
louko <w.riosmagalhaes@gmail.com>
2022-08-07 14:11:13 -0300

    Concatenação

---
+++ src/concatenacao/ComoConcatenar.java
@@ -0,0 +1,16 @@
+package concatenacao;
+
+public class ComoConcatenar {
+
+	public static void main(String[] args) {
+/* Concatenação é Unir ou juntar os dados*/
+		String  nome = "Wallace Magalhães";
+		String cpf = "111.677.417-81";
+		String telefone = "021 97408-1922";
+		String endereço = "Rua Tiradentes n 90";
+		System.out.println("Meu nome é: " + nome + "e o CPF:" + cpf + "meu contato é : " + telefone + "eu moro " +   endereço);
+		
+		
+	}
+
+}
